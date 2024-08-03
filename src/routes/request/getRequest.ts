f0a4ec19ca4b26dc24ba2a65690130898fa0c45b
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-08-03 23:19:59 +0200

    Utils for user route

--- src/routes/request/getRequest.ts
+++ src/routes/request/getRequest.ts
@@ -3,7 +3,6 @@ import { Request, Response,Application } from 'express';
 import { requestCrud } from '../../models/crud';
 import { classByNewer }  from '../../utils';
 
-
 export const getRequestByNewer = (app: Application) => {
   app.get(`${requestCrud.route}_token`, async (req: Request, res: Response) => {
       const item = await getCrudByIdInToken(res,req,requestCrud);
--- src/routes/request/postRequest.ts
+++ src/routes/request/postRequest.ts
@@ -2,7 +2,6 @@ import { createFunctionId } from "../utilsCrudUser";
 import { Request, Response,Application } from 'express';
 import { requestCrud } from '../../models/crud';
 
-
 export const postRequest = (app: Application) => {
   app.post(`${requestCrud.route}_id`, async (req: Request, res: Response) => {
       createFunctionId(res,req,requestCrud);
