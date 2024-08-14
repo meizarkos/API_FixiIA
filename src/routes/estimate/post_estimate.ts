e69eec4011b54df99741e080a3ae6e34033d9bae
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-08-14 23:48:34 +0200

    Add of post estimate

--- src/routes/estimate/post_estimate.ts
+++ src/routes/estimate/post_estimate.ts
@@ -3,7 +3,9 @@ import { Request, Response, Application } from 'express';
 import { estimateCrud } from '../../models/crud';
 
 export const postEstimate = (app: Application) => {
-    app.post(`${estimateCrud.route}_id`, async (req: Request, res: Response) => {
+    app.post(`${estimateCrud.route}_id/:user_id/:request_id`, async (req: Request, res: Response) => {
+        req.body.user_id = req.params.user_id;
+        req.body.request_id = req.params.request_id;
         createFunctionId(res, req, estimateCrud);
     });
 };
