b32411f5b0ef2ddcc97eb896836f36a605d6d29f
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-08-21 22:52:08 +0200

    Add of update request

--- src/routes/estimate/post_estimate.ts
+++ src/routes/estimate/post_estimate.ts
@@ -1,7 +1,7 @@
 import { createFunctionId } from '../utilsCrudUser';
 import { Request, Response, Application } from 'express';
 import { estimateCrud } from '../../models/crud';
-import { TimingEstimate } from '../../models';
+import { TimingEstimate, Request  as RequestModel } from '../../models';
 import { Model } from 'sequelize';
 
 export const postEstimate = (app: Application) => {
@@ -20,6 +20,13 @@ export const postEstimate = (app: Application) => {
                 await TimingEstimate.create({ time: timeParse, estimate_id: estimate.getDataValue('uuid') });
             })
         );
+
+        
+        await RequestModel.increment(
+            { number_of_estimate:  1 },
+            { where: { uuid: req.params.id } }
+        );
+
         res.status(201).json({
             message: `Item created in ${estimateCrud.route} and timing created in timing_estimate`
         });
--- src/routes/utilsCrudUser/patch.ts
+++ src/routes/utilsCrudUser/patch.ts
@@ -3,13 +3,13 @@ import { CrudAdmin } from '../../models/crud';
 import { getAllErrors } from '../../utils';
 import { ValidationError, UniqueConstraintError, Identifier } from 'sequelize';
 
-export async function patchUser(res: Response, req: Request, config: CrudAdmin, id: Identifier) {
+export async function patchUser(res: Response, req: Request, config: CrudAdmin, id: Identifier, returnElement = false) {
     if (config.patch !== undefined && config.patch === false) {
         res.status(500).json({
             error: 'Error in the server',
             message: 'You aren t suppose to use this model like this'
         });
-        return;
+        return null;;
     }
 
     try {
@@ -22,24 +22,27 @@ export async function patchUser(res: Response, req: Request, config: CrudAdmin, 
                 error: 'Bad Request',
                 message: 'Provided attributes are not valid or allowed.'
             });
-            return;
+            return null;;
         }
 
         const item = await config.model.findByPk(id);
 
         if (!item) {
             res.status(404).json({ message: `Item not found in ${config.route}` });
-            return;
+            return null;;
         }
 
-        await config.model.update(req.body, { where: { uuid: id } });
+        const updateElement = await config.model.update(req.body, { where: { uuid: id } });
+        if(returnElement === true) {
+            return updateElement;
+        }
         res.status(200).json({ message: `Item updated in ${config.route}` });
     } catch (e: unknown) {
         const attributes = Object.keys(config.model.getAttributes());
 
         if (e instanceof ValidationError || e instanceof UniqueConstraintError) {
             getAllErrors(e, res, attributes);
-            return;
+            return null;;
         }
 
         console.error(e);
