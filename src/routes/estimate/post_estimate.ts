8efdfdcd4bc3dbe4251c89a7e074630abd3cefc6
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2024-08-21 19:13:28 +0200

    Try post

--- src/routes/estimate/post_estimate.ts
+++ src/routes/estimate/post_estimate.ts
@@ -4,6 +4,8 @@ import { estimateCrud } from '../../models/crud';
 
 export const postEstimate = (app: Application) => {
     app.post(`${estimateCrud.route}_id`, async (req: Request, res: Response) => {
+        const timings = req.body.timing;
+        req.body.timing = undefined;
         const estimate = await createFunctionId(res, req, estimateCrud, true);
         if (estimate === null) return;
         res.status(201).json({ message: `Item created in ${estimateCrud.route}`, item: estimate });
